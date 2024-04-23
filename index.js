bc694c8de57755e117c8efa80959d391e31479f2
unknown <Superadmin@L-PNR23-1011.SUPERMARKET.local>
2024-04-23 11:25:13 +0100

    login2

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-
-//btn de login et register
-
 var x = document.getElementById('login');
 var y = document.getElementById('register');
 var z = document.getElementById('btn');
-
 function login(){
     x.style.left = "27px";
     y.style.right = "-350px";
@@ -15,42 +11,36 @@ function register(){
     y.style.right = "25px";
     z.style.left = "150px";
 }
+// View Password codes
+ 
 
-// btn pour voir le mot de passe
-         
-      
 function myLogPassword(){
+ var a = document.getElementById("logPassword");
+ var b = document.getElementById("eye");
+ var c = document.getElementById("eye-slash");
+ if(a.type === "password"){
+    a.type = "text";
+    b.style.opacity = "0";
+    c.style.opacity = "1";
+ }else{
+    a.type = "password";
+    b.style.opacity = "1";
+    c.style.opacity = "0";
+ }
+}
+function myRegPassword(){
 
-    var a = document.getElementById("logPassword");
-    var b = document.getElementById("eye");
-    var c = document.getElementById("eye-slash");
+ var d = document.getElementById("regPassword");
+ var b = document.getElementById("eye-2");
+ var c = document.getElementById("eye-slash-2");
 
-    if(a.type === "password"){
-       a.type = "text";
-       b.style.opacity = "0";
-       c.style.opacity = "1";
-    }else{
-       a.type = "password";
-       b.style.opacity = "1";
-       c.style.opacity = "0";
-    }
-
-   }
-
-   function myRegPassword(){
-
-    var d = document.getElementById("regPassword");
-    var b = document.getElementById("eye-2");
-    var c = document.getElementById("eye-slash-2");
-
-    if(d.type === "password"){
-       d.type = "text";
-       b.style.opacity = "0";
-       c.style.opacity = "1";
-    }else{
-       d.type = "password";
-       b.style.opacity = "1";
-       c.style.opacity = "0";
-    }
-
-   }
+ if(d.type === "password"){
+    d.type = "text";
+    b.style.opacity = "0";
+    c.style.opacity = "1";
+ }else{
+    d.type = "password";
+    b.style.opacity = "1";
+    c.style.opacity = "0";
+ }
+}
